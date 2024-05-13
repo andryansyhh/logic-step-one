f3ff0610b70bb25c44528abdb07fe55436a319cf
andryan.syah <asyah@pvg.co.id>
2024-05-14 02:23:09 +0700

    main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+func main() {
+	// hapus komen sesuai soal yang ingin di jalankan
+
+	//// soal 1
+	// fmt.Println(MatchStrings())
+
+	//// soal 2
+	// result, success := CalculateChange()
+	// if success {
+	// 	fmt.Println("Output:")
+	// 	fmt.Println(result)
+	// } else {
+	// 	fmt.Println(result)
+	// }
+
+	//// soal 3
+	// if isValid() {
+	// 	fmt.Println("TRUE")
+	// } else {
+	// 	fmt.Println("FALSE")
+	// }
+
+	////soal 4
+	// eligible, reason := checkLeaveEligibility()
+	// // Output hasil
+	// fmt.Println("Hasil:")
+	// if eligible {
+	// 	fmt.Println("True")
+	// } else {
+	// 	fmt.Println("False")
+	// 	fmt.Println("Alasan:", reason)
+	// }
+}
